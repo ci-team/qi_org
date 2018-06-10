7124d96b5945e357b8b748975b8d285821152807
Aleksandra Fedorova (bookwar) <alpha@bookwar.info>
2018-06-10 18:17:34 +0200

    Clean up base template

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,10 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+        ('You can add links in your config file', '#', 'fab fa-twitter'),
+        ('Another social link', '#', 'fab fa-guthub'),
+)
 
 DEFAULT_PAGINATION = 10
 
